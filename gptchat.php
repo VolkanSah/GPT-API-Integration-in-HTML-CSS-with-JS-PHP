d37435113243553eebb0fba90d8618ef5cadc692
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-16 21:04:47 +0100

    Create  gptchat.php

---
+++ gptchat.php
@@ -0,0 +1,21 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+// Replace YOUR_API_KEY with your actual GPTChat API key
+define('API_KEY', 'YOUR_API_KEY');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $input = json_decode(file_get_contents('php://input'), true);
+    $message = urlencode($input['message']);
+
+    $url = "https://api.gptchat.com/v1/engines/gpt-4/complete?api_key=" . API_KEY . "&message=" . $message;
+
+    $response = file_get_contents($url);
+    echo $response;
+} else {
+    http_response_code(405);
+    echo json_encode(['error' => 'Method not allowed']);
+}

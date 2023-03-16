2fe6adcd2d949af259581864dec867a05a93a6c0
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-16 21:05:56 +0100

    Create style.css

---
+++ style.css
@@ -0,0 +1,66 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+}
+
+.chat-container {
+    width: 100%;
+    max-width: 600px;
+    margin: 40px auto;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 3px 10px rgba(0,0, 0, 0.1);
+display: flex;
+flex-direction: column;
+}
+
+.chat-output {
+flex-grow: 1;
+padding: 20px;
+overflow-y: auto;
+max-height: 400px;
+}
+
+.chat-output p {
+margin: 10px 0;
+}
+
+.user-message {
+text-align: right;
+font-weight: bold;
+}
+
+.bot-message {
+text-align: left;
+font-weight: normal;
+}
+
+.chat-input-form {
+display: flex;
+align-items: center;
+padding: 10px;
+background-color: #f0f0f0;
+border-top: 1px solid #e0e0e0;
+}
+
+.chat-input {
+flex-grow: 1;
+border: none;
+border-radius: 3px;
+padding: 10px;
+margin-right: 10px;
+}
+
+.chat-submit {
+padding: 10px 20px;
+background-color: #007bff;
+color: #fff;
+font-weight: bold;
+border: none;
+cursor: pointer;
+border-radius: 3px;
+}
+
+.chat-submit:hover {
+background-color: #0056b3;
+}

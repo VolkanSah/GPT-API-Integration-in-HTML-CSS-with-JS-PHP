04c8b36916ac5fb0748a99f1b11ba45db01115fd
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-17 17:49:30 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
 // config file
-
 // Set OpenAI-API key
 define('OPENAI_API_KEY', 'Your API here');
-
 // set Modellparameter
 define('MODEL', 'text-davinci-003');
 define('TEMPERATURE', 0.9);
@@ -11,3 +9,4 @@ define('MAX_TOKENS', 100);
 define('TOP_P', 1);
 define('FREQUENCY_PENALTY', 0.0);
 define('PRESENCE_PENALTY', 0.0);
+// end if ;)

780c361876e1d945d5d8602e6c0c8784f8767355
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-04-25 15:25:23 +0200

    Update gptchat.php

--- gptchat.php
+++ gptchat.php
@@ -1,17 +1,35 @@
 <?php
+// Set the content type of the response to JSON
 header('Content-Type: application/json');
+
+// Allow cross-origin requests from any domain
 header('Access-Control-Allow-Origin: *');
+
+// Allow the POST method for cross-origin requests
 header('Access-Control-Allow-Methods: POST');
+
+// Specify allowed headers for cross-origin requests, including Content-Type and Authorization
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
+// Include the configuration file
 require_once 'config.php';
 
+// Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    // Decode the JSON input and store it in a variable
     $input = json_decode(file_get_contents('php://input'), true);
+    
+    // URL-encode the message from the input
     $message = urlencode($input['message']);
+    
+    // Initialize a new cURL session
 
     $ch = curl_init();
-// Change API-Endpoint URL to your needs
+// Change the API endpoint URL according to your needs
+// For example, use /v1/chat/completions for GPT-4, GPT-4-0314, GPT-4-32k, GPT-4-32k-0314, GPT-3.5-turbo, and GPT-3.5-turbo-0301 models
+// Use /v1/completions for Lingua models like text-davinci-003, text-davinci-002, text-curie-001, text-babbage-001, and text-ada-001
+// See the readme.md file for more information
     curl_setopt($ch, CURLOPT_URL, "https://api.openai.com/v1/engines/" . MODEL . "/completions");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
@@ -23,16 +41,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         "frequency_penalty" => FREQUENCY_PENALTY,
         "presence_penalty" => PRESENCE_PENALTY
     )));
+    // Set the Content-Type and Authorization headers
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         "Content-Type: application/json",
         "Authorization: Bearer " . OPENAI_API_KEY
     ));
-
+// Execute the cURL session and store the response
     $response = curl_exec($ch);
+    // Output the response
     echo $response;
-
+// Close the cURL session
     curl_close($ch);
 } else {
+     // Set the HTTP response code to 405 (Method Not Allowed) if the request method is not POST
     http_response_code(405);
+    // Output an error message in JSON format
     echo json_encode(['error' => 'Method not allowed']);
 }
